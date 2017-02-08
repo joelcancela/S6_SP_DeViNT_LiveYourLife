a39342169f4d1cc6545aeb35c4032db6e0b64cd1
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-02-08 13:51:42 +0100

    Added maven project

---
+++ code/src/main/java/fr/unice/polytech/devint/g2projet3/App.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.devint.g2projet3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
