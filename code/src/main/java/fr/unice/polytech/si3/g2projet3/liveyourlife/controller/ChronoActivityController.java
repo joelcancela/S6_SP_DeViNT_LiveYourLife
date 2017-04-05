8c04e456e20b4a3d6d065f2b23a530f1efd07734
Antoine Dezarnaud <antoine.dezarnaud@etu.unice.fr>
2017-04-05 13:23:07 +0200

    merge avec  archi & fxml shuffle et chrono liés aux controllers

--- code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/controller/ChronoActivityController.java
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/controller/ChronoActivityController.java
@@ -1,10 +1,22 @@
 package fr.unice.polytech.si3.g2projet3.liveyourlife.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
 /**
- * Class x
+ * Controller of the Chrono Activity
  *
  * @author Joël CANCELA VAZ
  */
 public class ChronoActivityController extends ActivityController {
-    
+
+    @FXML
+    public Text activityName;
+    @FXML
+    public ListView availableActions;
+    @FXML
+    public ListView pickedActions;
+
+
 }
