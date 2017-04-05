6cd5d47f69f56124d6cc0d504c418f1d6037f50e
antoined73 <dezartoine@hotmail.fr>
2017-04-05 10:52:35 +0200

    debut

--- code/src/main/java/dvt/exemple/chronologie/ChronoActivity.java
+++ code/src/main/java/dvt/exemple/chronologie/ChronoActivity.java
@@ -45,6 +45,7 @@ public class ChronoActivity extends JeuDevint {
             control = loader.getController();
             control.setScene(sc);
 
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
