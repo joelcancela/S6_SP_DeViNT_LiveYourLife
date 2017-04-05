9cac25461a015fc8109eaff9b3e592de5d039847
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-04-05 11:06:19 +0200

    Added first classes

---
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/ActivityMenu.java
@@ -0,0 +1,30 @@
+package fr.unice.polytech.si3.g2projet3.liveyourlife;
+
+import dvt.devint.menu.MenuDevint;
+
+/**
+ * Class x
+ *
+ * @author Joël CANCELA VAZ
+ */
+public class ActivityMenu extends MenuDevint {
+    /**
+     * le titre du menu
+     *
+     * @return le titre
+     */
+    @Override
+    public String titre() {
+        return null;
+    }
+
+    /**
+     * Permet d'initialiser le menu
+     * doit faire des appels à
+     * control.addmenuItems(label,action)
+     */
+    @Override
+    public void initMenu() {
+
+    }
+}
---
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/controller/ShuffleActivityController.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.si3.g2projet3.liveyourlife.controller;
+
+/**
+ * Class x
+ *
+ * @author Joël CANCELA VAZ
+ */
+public class ShuffleActivityController extends ActivityController {
+}
