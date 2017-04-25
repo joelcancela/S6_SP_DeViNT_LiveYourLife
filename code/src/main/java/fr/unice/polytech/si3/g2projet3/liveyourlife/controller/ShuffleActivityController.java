5c17d86dbd68bbf632f0863aad4469d355d56b38
Jeremy Junac <jeremy.junac@etu.unice.fr>
2017-04-25 09:35:13 +0200

    Begining of shuffle. Problem: Some field in Activity must be in ChronoActivity

--- code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/controller/ShuffleActivityController.java
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/controller/ShuffleActivityController.java
@@ -1,8 +1,22 @@
 package fr.unice.polytech.si3.g2projet3.liveyourlife.controller;
 
+import fr.unice.polytech.si3.g2projet3.liveyourlife.model.action.ChronoAction;
+import fr.unice.polytech.si3.g2projet3.liveyourlife.model.action.ShuffleAction;
+import fr.unice.polytech.si3.g2projet3.liveyourlife.model.activity.ChronoActivity;
+import fr.unice.polytech.si3.g2projet3.liveyourlife.model.activity.ShuffleActivity;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 /**
  * Class x
@@ -17,11 +31,15 @@ public class ShuffleActivityController extends ActivityController {
     @FXML
     public ImageView currentStateImage;
     @FXML
+    public ListView<ChronoAction> availableActions;
+    @FXML
     public Text actionType;
 
     @Override
     protected void init() {
-
+        activityName.setText(((ShuffleActivity) model).getTitle());
+        ((ShuffleActivity) model).setSIVOXInstance(scene.getSIVox());
+        ObservableList<ShuffleAction> possibleActions =  ((ShuffleActivity) model).getPossibleChoices();
     }
 
     @Override
@@ -31,6 +49,38 @@ public class ShuffleActivityController extends ActivityController {
 
     @Override
     public void mapTouchToActions() {
+        scene.mapKeyPressedToConsumer(KeyCode.LEFT, (x) -> left());
+        scene.mapKeyPressedToConsumer(KeyCode.RIGHT, (x) -> right());
+        scene.mapKeyPressedToConsumer(KeyCode.SPACE, (x) -> choose());
+    }
+    private void choose() {
+        int newIndex = ((ShuffleActivity) model).answerSelectedAction();
+        availableActions.getSelectionModel().select(newIndex);
+        if(availableActions.getItems().isEmpty()){
+            Timeline timeline = new Timeline(new KeyFrame(
+                    Duration.millis(1000),
+                    ae -> scene.getSIVox().playText("bravo tu as réussi")));
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Parent rootNode = null;
+            try {
+                rootNode = fxmlLoader.load(getClass().getResourceAsStream("/fxml/win.fxml"));
+                getScene().setRoot(rootNode);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            timeline.play();
+        }
+    }
+
+    private void right() {
+        int newIndex = ((ShuffleActivity) model).chooseRight();
+        availableActions.getSelectionModel().select(newIndex);
+    }
+
+    private void left() {
+        int newIndex = ((ShuffleActivity) model).chooseLeft();
+        availableActions.getSelectionModel().select(newIndex);
 
     }
+
 }
--- code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/model/activity/ShuffleActivity.java
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/model/activity/ShuffleActivity.java
@@ -1,6 +1,8 @@
 package fr.unice.polytech.si3.g2projet3.liveyourlife.model.activity;
 
+import fr.unice.polytech.si3.g2projet3.liveyourlife.model.action.ChronoAction;
 import fr.unice.polytech.si3.g2projet3.liveyourlife.model.action.ShuffleAction;
+import t2s.SIVOXDevint;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,7 +15,9 @@ import java.util.Queue;
  */
 public class ShuffleActivity extends Activity<ShuffleAction> {
 
+    private SIVOXDevint sivoxDevint;
     private Queue<List<ShuffleAction>> nextChoices;
+    private int currentChoice = 0;
 
     public ShuffleActivity(String title, List<ShuffleAction> choices) {
         super(title, choices);
@@ -28,5 +32,35 @@ public class ShuffleActivity extends Activity<ShuffleAction> {
         }
 //        possibleChoices = nextChoices.remove();
         return false;
+    }
+
+    public int answerSelectedAction() {
+        if(possibleChoices.size()>0){
+            boolean wasCorrect = answer(possibleChoices.get(currentChoice));
+            if(wasCorrect){
+                currentChoice = Math.max(currentChoice-1,0);
+                sivoxDevint.playText("Bonne réponse !");
+            }
+            else{
+                sivoxDevint.playText("Mauvaise réponse !");
+            }
+        }
+        return currentChoice;
+    }
+
+    public int chooseRight() {
+        if (currentChoice < possibleChoices.size() - 1)
+            currentChoice++;
+        return currentChoice;
+    }
+
+    public int chooseLeft() {
+        if (currentChoice > 0)
+            currentChoice--;
+        return currentChoice;
+    }
+
+    public void setSIVOXInstance(SIVOXDevint sivoxDevint){
+        this.sivoxDevint = sivoxDevint;
     }
 }
