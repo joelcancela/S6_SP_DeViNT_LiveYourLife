ff8ef6ccb91c5d3d52b1cccb775e42a44d813b30
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-05-20 19:19:14 +0200

    Updated livraison

--- livraison/liveyourlife/bin/execution.bat
+++ livraison/liveyourlife/bin/execution.bat
@@ -1,4 +1,4 @@
 set PATH=../../lib/;../../jre/bin/;../ressources
 rem set PATH=%PATH%../../lib/;../../jre/bin/;../ressources
-java -jar LiveYourLife_DeViNT.jar -Dfile.encoding=utf-8
+java -jar LiveYourLife.jar -Dfile.encoding=utf-8
 pause
