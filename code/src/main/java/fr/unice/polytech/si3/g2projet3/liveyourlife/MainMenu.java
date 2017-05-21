269978fa792e098c5b6cedf8365e070f4d3fb136
Antoine Dezarnaud <antoine.dezarnaud@etu.unice.fr>
2017-05-21 16:56:37 +0200

    takeAShower done

--- code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/MainMenu.java
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/MainMenu.java
@@ -3,6 +3,7 @@ package fr.unice.polytech.si3.g2projet3.liveyourlife;
 import dvt.devint.menu.MenuDevint;
 import fr.unice.polytech.si3.g2projet3.liveyourlife.common.BrushTeethGame;
 import fr.unice.polytech.si3.g2projet3.liveyourlife.common.DressUnderSun;
+import fr.unice.polytech.si3.g2projet3.liveyourlife.common.TakeAShower;
 import fr.unice.polytech.si3.g2projet3.liveyourlife.common.WashHandsGame;
 import javafx.application.Application;
 
@@ -35,6 +36,7 @@ public class MainMenu extends MenuDevint {
     public void initMenu() {
 //        control.addMenuItem("Jouer une journée complète", x-> new ShuffleActivityController());
         control.addMenuItem("Se brosser les dents", x-> new BrushTeethGame());
+        control.addMenuItem("Se doucher", x-> new TakeAShower());
         control.addMenuItem("Se laver les mains", x-> new WashHandsGame());
         control.addMenuItem("S'habiller", x-> new DressUnderSun());
 //        control.addMenuItem("S'habiller", x-> new ShuffleActivityController());
