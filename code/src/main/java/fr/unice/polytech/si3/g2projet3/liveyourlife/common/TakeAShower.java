b12287790e042f870504ccd324f3f5b475ef4292
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-05-21 16:12:05 +0200

    Added games classes

---
+++ code/src/main/java/fr/unice/polytech/si3/g2projet3/liveyourlife/common/TakeAShower.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.si3.g2projet3.liveyourlife.common;
+
+/**
+ * {@code TakeAShower} is [desc]
+ * <p>
+ * [descSuite]
+ *
+ * @author Joël CANCELA VAZ
+ */
+public class TakeAShower {
+}
